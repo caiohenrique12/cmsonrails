0ec7fda5baa3149469da4389174da1d097d7b5d0
chenrique <caiohenriqueads@gmail.com>
2015-10-15 21:34:09 -0300

    varias mudanças

--- app/models/conteudo.rb
+++ app/models/conteudo.rb
@@ -1,5 +1,5 @@
 class Conteudo < ActiveRecord::Base
-  has_attached_file :image, styles: { large: "600x600", medium: "300x300", thumb: "150x150" }
+  has_attached_file :image, styles: { large: "1120x480", medium: "300x300", thumb: "150x150" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
