23c019ade490acbf9747727349de2671c8afd304
chenrique <caiohenriqueads@gmail.com>
2015-10-16 17:55:24 -0300

    configurando inflections para categoria e sugestao

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,12 +6,13 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
+  inflect.irregular 'categoria', 'categorias'
+  inflect.irregular 'sugestao', 'sugestoes'
 #   inflect.uncountable %w( fish sheep )
 # end
 
 # These inflection rules are supported but not enabled by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
-inflect.uncountable %w( fish sheep home)
+  inflect.uncountable %w( fish sheep home)
 end
